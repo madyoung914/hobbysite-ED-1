95da28f08ee24f3509ecec3bdcf54cdd2b2c7700
Mady <youngmadeleineh@gmail.com>
2025-02-27 21:20:18 +0800

    made models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    #to have product types in ascending order i think ???
+    class Meta:
+        ordering = ['name']
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    #idk abt the max digits ??
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    producttype = models.ForeignKey(ProductType,
+                                    on_delete=models.SET_NULL,
+                                    null=True,
+                                    related_name= "type")
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger: merch-detail', args=[self.pk])
+    
+    class Meta:
+        ordering = ['name']
+
+
+            
+
