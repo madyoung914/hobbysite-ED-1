0821d918ff7363d1f8bdef0760165b3a8dbe12e4
alyssaheh <maegan.tan@student.ateneo.edu>
2025-02-28 16:15:39 +0800

    Created the Wiki App

---
+++ hobbysite/wiki/urls.py
@@ -0,0 +1,10 @@
+# <appname>/urls.py
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+]
+
+# This might be needed, depending on your Django version
+app_name = "wiki"
