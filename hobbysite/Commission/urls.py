0f7c8bcdd6b4262a09346f6160166bb63d904f3b
Kino De Torres <ken.detorres@student.ateneo.edu>
2025-03-03 02:12:44 +0800

    Debugged, CreatedAdmin, Added to database

--- hobbysite/Commission/urls.py
+++ hobbysite/Commission/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import 
+from .views import CommissionListView, CommissionDetailView
+
 urlpatterns = [
-	path('commission/list', commissionListView, name='commissionList'),
-	path('commission/detail/<int:pk>', commissionDetailView, name='commissionDetail'),
+	path('commission/list', CommissionListView.as_view(), name='commissionList'),
+	path('commission/detail/<int:pk>', CommissionDetailView.as_view(), name='commissionDetail'),
 ]
 app_name = "Commission"
