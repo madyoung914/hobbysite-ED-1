92de9d489a1705891aa5307db956f4d4e9d84f4e
alyssaheh <maegan.tan@student.ateneo.edu>
2025-03-02 13:18:21 +0800

    Created templates, and working list and detail view

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,9 +1,10 @@
 # <appname>/urls.py
 from django.urls import path
-from .views import index
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('wiki/articles', ArticleListView.as_view(), name='articles-list'),
+    path('wiki/article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
 
 # This might be needed, depending on your Django version
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Article
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello World! This came from the index view')
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
 
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
