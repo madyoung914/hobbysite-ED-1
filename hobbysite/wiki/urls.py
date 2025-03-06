5e341a6d7b9133a697563161ca2fa245fe99175c
alyssaheh <maegan.tan@student.ateneo.edu>
2025-03-06 11:59:28 +0800

    Placed the templates inside wiki folder, and fixed urls

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('wiki/articles/', ArticleListView.as_view(), name='articles-list'),
-    path('wiki/article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('wiki/articles', ArticleListView.as_view(), name='articles-list'),
+    path('wiki/article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
 
 app_name = "wiki"
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -4,9 +4,9 @@ from .models import Article
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'wiki/article_list.html'
 
 
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'article_detail.html'
+    template_name = 'wiki/article_detail.html'
