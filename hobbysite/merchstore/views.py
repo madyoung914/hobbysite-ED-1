b44989bd0c3f384199c1ed810e082b0eeb4f4372
Mady <youngmadeleineh@gmail.com>
2025-04-17 23:10:18 +0800

    updated everything

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Product, ProductType
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProductForm
 
 
 class ProductTypeListView(ListView):
@@ -12,3 +15,9 @@ class ProductTypeListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = "merchstore/merch_detail.html"
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "merchstore/merch_add.html"
