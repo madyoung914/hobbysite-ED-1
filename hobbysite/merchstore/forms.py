f4860d9eb54f4b01a5a3778a367d988a15f98c8d
Mady <youngmadeleineh@gmail.com>
2025-04-18 23:41:57 +0800

    did half of detail view and update view

--- hobbysite/merchstore/forms.py
+++ hobbysite/merchstore/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Product
+from .models import Product, Transaction
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        exclude = ['buyer', 'product', 'status', ]
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .models import Product, ProductType
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import ProductForm
+from .forms import ProductForm, TransactionForm
 
 
 class ProductTypeListView(ListView):
@@ -12,12 +13,39 @@ class ProductTypeListView(ListView):
     template_name = "merchstore/merch_list.html"
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
     template_name = "merchstore/merch_detail.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['form'] = TransactionForm()
+        return ctx
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = TransactionForm(request.POST)
+
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.product = self.object
+            transaction.buyer = request.user.profile
+            transaction.status = 'CART'
+            transaction.save()
+            return redirect(reverse('merchstore:merchstore-item', kwargs={'pk': self.object.pk}))
+        else:
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
 
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = "merchstore/merch_add.html"
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "merchstore/merch_edit.html"
