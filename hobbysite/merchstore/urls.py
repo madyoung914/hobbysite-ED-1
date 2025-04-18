925e7e5b762b5df3de5e0238e0fc3b7238390cf3
Mady <youngmadeleineh@gmail.com>
2025-04-18 23:41:57 +0800

    did half of detail view and update view

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ProductCreateView, ProductTypeListView, ProductDetailView
+from .views import ProductCreateView, ProductTypeListView, ProductDetailView, ProductUpdateView
 
 urlpatterns = [
     path('items', ProductTypeListView.as_view(), name ="merchstore-list"),
     path('item/<int:pk>', ProductDetailView.as_view(),
          name="merchstore-item"),
     path('item/add', ProductCreateView.as_view(), name="merch-add"),
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name="merch-edit"),
 ]
 
 app_name = "merchstore"
--- hobbysite/user_management/admin.py
+++ hobbysite/user_management/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
+from merchstore.models import Transaction
 
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -10,7 +15,7 @@ class ProfileInline(admin.StackedInline):
 
 
 class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline,]
+    inlines = [ProfileInline, ]
 
 
 admin.site.unregister(User)
