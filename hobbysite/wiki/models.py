4e49ed4601d5a0123ced4a5406ec499138b51081
alyssaheh <maegan.tan@student.ateneo.edu>
2025-04-29 18:04:52 +0800

    Created models

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from user_management.models import Profile
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -15,12 +15,21 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile, 
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
         null=True
     )
     entry = models.TextField()
+    header_image = models.ImageField(
+        upload_to='headers/',
+        null=True
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -32,3 +41,24 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.article}'
+
+    class Meta:
+        ordering = ['created_on']
