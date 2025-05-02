df75d95dfbd45ade23c19a9679faffa828a6fbc3
Mady <youngmadeleineh@gmail.com>
2025-05-02 15:42:23 +0800

    tried doing the registration

---
+++ hobbysite/main_interface/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class CreateUserForm(UserCreationForm):
+    display_name = forms.CharField(max_length=63)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
--- hobbysite/main_interface/urls.py
+++ hobbysite/main_interface/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AppsListView
+from .views import AppsListView, UserCreateView
 
 urlpatterns = [
     path('', AppsListView.as_view(), name='homepage'),
+    path('register', UserCreateView.as_view(), name='register'),
 ]
 
 app_name = "main_interface"
