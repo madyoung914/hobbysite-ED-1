0e4ec383b3897be9cfb0847bb128a89d12977e38
Kino De Torres <kendetorres2105@gmail.com>
2025-05-03 09:44:20 +0800

    Updating Models

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -3,8 +3,19 @@ from django.db import models
 from django.urls import reverse
 
 class Commission(models.Model):
+    
+    class OrderStatus(models.TextChoices):
+        OPEN = 'O', 'Open'
+        FULL = 'F', 'Full'
+        COMPLETE = 'C', 'Complete'
+        Discontinued = 'D', 'Discontinued'
+    
     title = models.CharField(max_length=255)
     description = models.TextField()
+    status = models.CharField(
+        choices = OrderStatus.choices,
+        default = OrderStatus.OPEN
+        )
     peopleRequired = models.IntegerField()
     CreatedOn = models.DateTimeField(auto_now_add=True)
     UpdatedOn = models.DateTimeField(auto_now=True)
@@ -26,6 +37,46 @@ class Comment(models.Model):
         related_name='comments'
     )
     entry = models.TextField()
+    CreatedOn = models.DateTimeField(auto_now_add=True)
+    UpdatedOn = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-UpdatedOn']
+
+class Job(models.Model):
+    commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    role = models.CharField(max_length=255)
+    manpowerRequired = models.IntegerField()
+    status = models.CharField(default='Open')
+    
+    def updateStatus(self,numApps):
+        if numApps==self.manpowerRequired:
+            self.status = 'Full'
+            self.save()
+    CreatedOn = models.DateTimeField(auto_now_add=True)
+    UpdatedOn = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-UpdatedOn']
+
+class Job(models.Model):
+    commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    role = models.CharField(max_length=255)
+    manpowerRequired = models.IntegerField()
+    status = models.CharField(default='Open')
+    
+    def updateStatus(self,numApps):
+        if numApps==self.manpowerRequired:
+            self.status = 'Full'
+            self.save()
     CreatedOn = models.DateTimeField(auto_now_add=True)
     UpdatedOn = models.DateTimeField(auto_now=True)
 
