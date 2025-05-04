da4bd9f761f82151b50a8ec635b3b1669c46559f
Wahab-a-web <abdul.iburahim@student.ateneo.edu>
2025-05-04 21:49:07 +0800

    threads separated by categories

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,11 +1,32 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Thread
+from .models import Thread, ThreadCategory
+from django.db.models import Prefetch
 
 
 class ThreadListView(ListView):
     model = Thread
     template_name = 'forum/thread_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_authenticated:
+            user_profile = self.request.user.profile
+            user_threads = Thread.objects.filter(author = user_profile)
+            other_threads = Thread.objects.exclude(author = user_profile)
+        else:
+            user_threads = Thread.objects.none()
+            other_threads = Thread.objects.all()         
+
+        categories = ThreadCategory.objects.all().prefetch_related(
+            Prefetch('thread_set', queryset = other_threads)
+        )
+
+        context['user_threads'] = user_threads
+        context['grouped_threads'] = categories
+        return context
 
 
 class ThreadDetailView(DetailView):
