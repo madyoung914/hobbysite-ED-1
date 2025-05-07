1288472f537a76c42e086aa9d5cffe7c24086f24
alyssaheh <maegan.tan@student.ateneo.edu>
2025-05-07 08:34:25 +0800

    Added image gallery to admin panel

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Article, ArticleCategory
+from .models import Article, ArticleCategory, ImageGallery
 
+class ImageGalleryInline(admin.TabularInline):
+    model = ImageGallery
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
     list_display = ('title', 'category', 'created_on', 'updated_on')
     list_filter = ('category', 'created_on', 'updated_on')
     search_fields = ('title',)
-
+    inlines = [ImageGalleryInline]
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
