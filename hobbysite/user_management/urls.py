8c2bb85f94e4d0886c7c1ed9b430716a7104e344
Mady <youngmadeleineh@gmail.com>
2025-05-14 23:03:12 +0800

    fixed dashboard

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ProfileDetailView, ProfileUpdateView, ProfileTemplateView
+from .views import ProfileDetailView, ProfileUpdateView
 
 urlpatterns = [
     path('profile/<str:username>', ProfileDetailView.as_view(), name='profile'),
     path('profile/<str:username>/edit', ProfileUpdateView.as_view(), name='profile-edit'),
-    path('profile/<str:username>/dashboard', ProfileTemplateView.as_view(), name='profile-dashboard'),    
 ]
 
 app_name = "user_management"
