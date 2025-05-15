81ce58a158e128467eda2c2f7b98ba8127a4e5f8
Cobble-Gen <ianmercadopvt@gmail.com>
2025-05-15 19:41:35 +0800

    Edited static_root

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from .cdn.conf import * #noqa
 
