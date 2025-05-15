95df7e08be9f6e8df5c45306ec5ba00ba8402724
Cobble-Gen <ianmercadopvt@gmail.com>
2025-05-15 17:25:49 +0800

    Edited static root

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 from .cdn.conf import * #noqa
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, 'hobbysite-static')
+STATIC_URL = 'AWS_S3_ENDPOINT_URL'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
