b0b19fa784f77f39abc08f7bcab20e478707266f
Cobble-Gen <ianmercadopvt@gmail.com>
2025-05-15 18:30:24 +0800

    Edited settings

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = '5Npq/S/7tX2IqBl51p3QEAMJuuGvHuFH4680Cl59M3s'
